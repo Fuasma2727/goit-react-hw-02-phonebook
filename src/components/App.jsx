ffba4b4ccc50ebe7430a1411dd98527eb396217b
Fuasma2727 <jmamgb127@gmail.com>
2023-09-28 21:34:35 -0500

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import ContactForm from "./ContactForm"
+import Filter from "./Filter"
+
+
+
+export const App = () => {
+  const [contacts, setContacts] = useState([]);
+  const [filter, setFilter] = useState('');
+
+  const handleAddContact = (newContact) => {
+    if (contacts.some((contact) => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
+      alert(`${newContact.name} is already in contacts.`);
+      return;
+    }
+    setContacts([...contacts, newContact]);
+  };
+  const handleDeleteContact = (id) => {
+    // Utiliza setContacts para actualizar el estado de los contactos
+    setContacts((prevContacts) => prevContacts.filter((contact) => contact.id !== id));
+  };
+  const handleFilterChange = (e) => {
+    setFilter(e.target.value);
+  };
+  const filteredContacts = contacts.filter((contact) =>
+  contact.name.toLowerCase().includes(filter.toLowerCase())
+);
+
+
+  return (
+    <div 
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+    <h1>Contact List</h1>
+<ContactForm addContact={handleAddContact} />
+<Filter value={filter} onChange={handleFilterChange} />
+<ul>
+{filteredContacts.map((contact) => (
+          <li key={contact.id}>
+            {contact.name} {contact.number}
+            <button onClick={() => handleDeleteContact(contact.id)}>Delete</button>
+          </li>
+        ))}
+</ul>
+</div>
+  );
+};
---
+++ src/components/Filter.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Filter = ({ value, onChange }) => (
+  <input
+    type="text"
+    placeholder="Search contacts"
+    value={value}
+    onChange={onChange}
+  />
+);
+
+export default Filter;
